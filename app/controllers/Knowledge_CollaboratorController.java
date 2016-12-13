3722ae53d6de4363d48f9736edc5cce5681e82ba
djewelMcDee <joelledepagne@hotmail.com>
2016-12-13 16:04:35 +0100

    rectification bdd

---
+++ app/controllers/Knowledge_CollaboratorController.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by Formation on 13/12/2016.
+ */
+public class Knowledge_CollaboratorController extends Controller {
+}
--- app/models/Knowledge.java
+++ app/models/Knowledge.java
@@ -4,9 +4,14 @@ import play.db.jpa.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 public class Knowledge extends Model {
     public String name;
+
+    @OneToMany(mappedBy = "knowledge")
+    public Set<Knowledge_Collaborator> knowledge_collaborators;
 
 }
