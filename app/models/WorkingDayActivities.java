0663561bfb1f3e2660a60f2e3090931ee58c91a3
Faiz <mr.ibrahim.faiz@gmail.com>
2016-12-13 09:48:46 +0100

    Ajout Membres de certaines classe

--- app/models/WorkingDayActivities.java
+++ app/models/WorkingDayActivities.java
@@ -6,4 +6,8 @@ import javax.persistence.Entity;
 
 @Entity
 public class WorkingDayActivities extends Model {
+    public Project project;
+    public Profile profile;
+    public enum Temps {
+    }
 }
